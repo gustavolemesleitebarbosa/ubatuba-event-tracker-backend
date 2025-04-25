6adeff5f4fa1238896551cffe7bd1921b1091f7c
Gustavo <pregustavomium@gmail.com>
2025-04-25 13:23:47 -0300

    feat: create project models

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
+from .database import Base
+
+class Event(Base):
+    __tablename__ = "events"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False)
+    description = Column(Text)
+    date = Column(DateTime, nullable=False)
+    location = Column(String(100), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now()) 
