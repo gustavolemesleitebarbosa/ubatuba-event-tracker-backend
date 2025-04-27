52bb0bd9841caaee3759bbbf4c2e0c06eeede917
Gustavo <pregustavomium@gmail.com>
2025-04-26 22:44:24 -0300

    feat: updata pooler use

--- app/database.py
+++ app/database.py
@@ -6,18 +6,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-import os
-
-# Get Supabase connection details from environment variables
 DB_USER = os.getenv("SUPABASE_DB_USER")
 DB_PASSWORD = os.getenv("SUPABASE_DB_PASSWORD")
 DB_HOST = os.getenv("SUPABASE_DB_HOST")
-DB_PORT = os.getenv("SUPABASE_DB_PORT", "5432")  # Default to 5432, the standard PostgreSQL port
+DB_PORT = os.getenv("SUPABASE_DB_PORT", "6543")
 DB_NAME = os.getenv("SUPABASE_DB_NAME")
+PROJECT_REF = DB_HOST.split('.')[1]  
 
-# Construct the database URL for direct connection (no Pooler)
 SQLALCHEMY_DATABASE_URL = (
-    f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?options=-c%20application_name%3D{PROJECT_REF}-pooler"
 )
 
 engine = create_engine(
